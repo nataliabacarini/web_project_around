87228ac7be37ad62566eb337d0aadcca017e4116
Natalia Bacarini <nataliabacarinib@gmail.com>
2024-05-10 19:42:50 -0300

    new update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,46 @@
 const popupForm = document.querySelector(".popup");
 const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".popup__close-button");
-
 const nameInput = document.querySelector(".popup__name");
 const aboutInput = document.querySelector(".popup__about");
-
 const nameUser = document.querySelector(".profile__name");
 const aboutUser = document.querySelector(".profile__about");
-
 const formSubmit = document.querySelector(".popup__form");
-
 const popupFormImage = document.querySelector(".popup__image");
 const addButtonImage = document.querySelector(".profile__add-picture-button");
 const closeButtonImage = document.querySelector(".popup__close-button-image");
-
 const titleImageInput = document.querySelector(".popup__title-image");
 const linkImageInput = document.querySelector(".popup__link-image");
-
-const addCard = document.querySelector(".gallery__cards");
+const cardsContainer = document.querySelector(".gallery__cards");
+const popupFormCard = document.querySelector(".popup__image-form");
+const popupViewer = document.querySelector(".viewer-image");
+const closeButtonViewer = document.querySelector(".viewer__close-button");
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
 
 function changeDisplayToBlock(popup, popupOpenedClass) {
   popup.classList.add(popupOpenedClass);
@@ -44,7 +67,7 @@ function saveNewInputValues(event) {
     nameUser.textContent = nameInput.value;
     aboutUser.textContent = aboutInput.value;
   }
-  changeDisplayToNone();
+  changeDisplayToNone(popupForm, "popup__opened");
 }
 
 formSubmit.addEventListener("submit", saveNewInputValues);
@@ -55,3 +78,63 @@ addButtonImage.addEventListener("click", () =>
 closeButtonImage.addEventListener("click", () =>
   changeDisplayToNone(popupFormImage, "popup__image-opened")
 );
+
+function createCard(card) {
+  const template = document.querySelector("#template");
+  const cardItem = template.content
+    .querySelector(".gallery__card")
+    .cloneNode(true);
+  cardItem.querySelector(".gallery__card-name").textContent = card.name;
+  cardItem.querySelector(".gallery__image").setAttribute("src", card.link);
+  cardItem.querySelector(".gallery__image").setAttribute("alt", card.name);
+  cardItem
+    .querySelector(".gallery__trash-button")
+    .addEventListener("click", function (event) {
+      const removeCard = event.target.parentElement;
+      removeCard.remove();
+    });
+  cardItem
+    .querySelector(".gallery__like-button")
+    .addEventListener("click", function (event) {
+      const likeButton = event.target;
+      if (likeButton.getAttribute("src") == "./images/like.svg") {
+        return likeButton.setAttribute("src", "./images/like_black.svg");
+      }
+      return likeButton.setAttribute("src", "./images/like.svg");
+    });
+  cardItem
+    .querySelector(".gallery__image")
+    .addEventListener("click", function (event) {
+      const image = event.target;
+      popupViewer.classList.add("viewer__image-opened");
+      const imageSource = image.getAttribute("src");
+      const imageAlt = image.getAttribute("alt");
+      const viewerPicture = document.querySelector(".viewer__picture");
+      viewerPicture.setAttribute("src", imageSource);
+      viewerPicture.setAttribute("alt", imageAlt);
+      const imageTitle = document.querySelector(".viewer__title");
+      imageTitle.textContent = card.name;
+      closeButtonViewer.addEventListener("click", () =>
+        changeDisplayToNone(popupViewer, "viewer__image-opened")
+      );
+    });
+  return cardItem;
+}
+
+for (const card of initialCards) {
+  cardsContainer.prepend(createCard(card));
+}
+
+function creatNewcard(event) {
+  const newCard = createCard({
+    name: titleImageInput.value,
+    link: linkImageInput.value,
+  });
+  cardsContainer.prepend(newCard);
+  changeDisplayToNone(popupFormImage, "popup__image-opened");
+}
+
+popupFormCard.addEventListener("submit", function (event) {
+  event.preventDefault();
+  creatNewcard();
+});
